2864b79f5c9160bfe09014b5454fb4cd17fd495b
Ian <152772a@mymail.nyp.edu.sg>
2018-06-16 23:46:05 +0800

    aysnc and callback functions

--- database/patientdb.js
+++ database/patientdb.js
@@ -1,27 +1,28 @@
 var dbcontroller = require('./database-config');
 
 
-
 module.exports =
-{   //function name:function(){...}
-    /**
-     * Get patient through patient id
-     * @param patientid
-     */
-    getPatientThruID: function(patientid)
-    {
-        console.log(patientid);
-        var query = "SELECT * from patient where patientid = ?"; 
-        dbcontroller.query(query,[patientid] ,function(err, rows) {
-            if (!err){
-                //convert to JSON format
-                var r = JSON.stringify(rows);
-                var result = JSON.parse(r);
-                console.log(result);
-              return result;
-            }
-            else
-              console.log('Patient ID not found');
-          });
-    }
-}
+    {   //function name:function(){...}
+        /**
+         * Get patient through patient id
+         * @param patientid
+         */
+        getPatientThruID: function (patientid, callback) {
+            console.log(patientid);
+            var query = "SELECT * from patient where patientid = ?";
+            dbcontroller.query(query, [patientid], function (err, rows) {
+                if (!err) {
+                    //convert to JSON format
+                    var r = JSON.stringify(rows);
+                    var result = JSON.parse(r);
+                    console.log(result);
+                    callback(null, result);
+                    // return result;
+                }
+                else
+                    console.log('Patient ID not found');
+            });
+        }
+
+
+    };
--- server.js
+++ server.js
@@ -9,16 +9,16 @@ var patientDB = require('./database/patientdb');
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 //need this for bodyparser
 var json_body_parser = bodyParser.json();
 
 //listen to server on port 3000
-app.listen(port,function() {
+app.listen(port, function () {
     console.log('Example app listening on port');
 });
 
-app.get('/',function(req,res) {
+app.get('/', function (req, res) {
     //res.send('Hello World');
     res.send("server open");
 
@@ -31,7 +31,7 @@ app.get('/',function(req,res) {
 });
 
 //webhook post function
-app.post('/webhook',json_body_parser,function(req,res){
+app.post('/webhook', json_body_parser, function (req, res) {
 
     console.log('webhook');
     //get session id to determine patient
@@ -41,19 +41,21 @@ app.post('/webhook',json_body_parser,function(req,res){
     //get contexts 
 
 
-
 });
 
-app.get('/getPatientThruID',function(req,res){
+app.get('/getPatientThruID', function (req, res) {
 
-        let patientId = req.query.patientId.toString();
+    var patientId = req.query.patientId.toString();
 
-        console.log(patientId);
-        let respond = patientDB.getPatientThruID(patientId);
-        console.log("sql resopond = "+respond);
-        res.send(respond);
+    console.log(patientId);
 
+    console.log("!@3");
+    patientDB.getPatientThruID("1", function (err, result) {
+        console.log(result);
 
+        res.send(result)
+
+    });
 
 
 });
@@ -63,17 +65,17 @@ app.get('/getPatientThruID',function(req,res){
 //AND DO RELAVENT CALLS TO OTHER API IF NEEDED
 
 /**
- * Get patient through the session id 
- * @param sessionid 
+ * Get patient through the session id
+ * @param sessionid
  */
 
-function getPatientIDThru(sessionid) {  
+function getPatientIDThru(sessionid) {
     //projects/epione-test/agent/intents/facd16aa-e3db-4de0-b26e-74f9c771e9b6
     //uncomment later
     //var str = req.body.session;
     var sessions = sessionid.split("/");
     //console.log("Patient ID: " + sessions[sessions.length - 1 ] );
-    var patientid = sessions[sessions.length - 1 ];
+    var patientid = sessions[sessions.length - 1];
     patientDB.getPatientThruID(patientid);
 }
 
